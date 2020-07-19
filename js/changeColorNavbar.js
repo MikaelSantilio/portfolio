0d570965921dd272c1d164e509c3fec887731d24
MikaelSantilio <mikael.santilio@gmail.com>
2020-07-19 17:59:33 -0300

    First semantic tags markup, removed old files

--- js/changeColorNavbar.js
+++
@@ -1,6 +0,0 @@
-$(function () {
-    $("body").scroll(function () {
-      var $nav = $(".navbar-transparente");
-      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-    });
-  });
